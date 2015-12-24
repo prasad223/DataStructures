bc82d4a52d4347c375d0a041001bd1ae0a7a5ce7
prasad223 <prasadsjcecs@gmail.com>
2015-12-23 23:40:53 -0500

    Added test cases for basic linked list class

---
+++ Tests/BasicDS/LinkedLists/LinkedListTest.java
@@ -0,0 +1,60 @@
+package BasicDS.LinkedLists;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by prasad223 on 12/23/15.
+ */
+public class LinkedListTest extends TestCase {
+
+    LinkedList linkedList;
+    public void setUp() throws Exception {
+        super.setUp();
+        linkedList = new LinkedList();
+
+    }
+
+    public void tearDown() throws Exception {
+        linkedList = null;
+    }
+
+
+    public void testInsertFront() throws Exception {
+        linkedList.insertFront(5);
+        assertEquals(linkedList.getHead(), 5);
+        linkedList.insertFront(7);
+        assertEquals(linkedList.getHead(),7);
+    }
+
+    public void testInsertRear() throws Exception {
+        linkedList.insertRear(4);
+        assertEquals(linkedList.getTail(), 4);
+        assertEquals(linkedList.getHead(), 4);
+        linkedList.insertRear(6);
+        assertEquals(linkedList.getTail(), 6);
+    }
+
+    public void testGetHead() throws Exception {
+        try{
+            linkedList.getHead();
+        }catch (Exception e){
+            if(e instanceof NullPointerException){
+                assertEquals(true, true);
+            }
+        }
+
+        linkedList.insertFront(5);
+        assertEquals(linkedList.getHead(), 5);
+    }
+
+    public void testSearch() throws Exception {
+        assertEquals(linkedList.search(5), -1);
+        linkedList.insertFront(6);
+        assertEquals(linkedList.search(6), 0);
+        linkedList.insertRear(4);
+        linkedList.insertFront(5);
+        assertEquals(linkedList.search(6),1);
+        assertEquals(linkedList.search(10), -1);
+    }
+
+}
---
+++ src/BasicDS/LinkedLists/LinkedList.java
@@ -0,0 +1,102 @@
+package BasicDS.LinkedLists;
+
+/**
+ * Created by prasad223 on 12/23/15.
+ */
+
+import java.lang.NullPointerException;
+
+/**
+ * This is the basic node class for Linked List data structure
+ */
+class LLNode{
+
+    public int data;
+    public LLNode next;
+
+    public LLNode(int data, LLNode next){
+        this.data = data;
+        this.next = next;
+    }
+
+    public LLNode(){
+        new LLNode(-1,null);
+    }
+
+    public LLNode(int data){
+        this.data = data;
+        this.next = null;
+        //new LLNode(data, null);
+    }
+}
+
+/**
+ * Basic LinkedList data structure class
+ */
+public class LinkedList {
+
+    private LLNode head, tail;
+
+    public  LinkedList(){
+        this.head = null;
+        this.tail = null;
+    }
+
+    public LinkedList(int data){
+        this.head = new LLNode(data);
+        this.tail = this.head;
+    }
+
+    public void insertFront(int data){
+        LLNode newNode = new LLNode(data, this.head);
+        this.head = newNode;
+        if(this.tail == null){
+            this.tail = newNode;
+        }
+    }
+
+    public void insertRear(int data){
+        LLNode node = new LLNode(data);
+        if(this.head == null && this.tail == null){
+            this.head = this.tail = node;
+            return;
+        }
+        if(this.tail != null){
+            this.tail.next = node;
+            this.tail = node;
+        }
+    }
+
+    public  int getTail(){
+        if(this.tail != null){
+            return tail.data;
+        }
+        throw new NullPointerException("Empty tail");
+    }
+
+    public int getHead(){
+
+        if(this.head != null){
+            return this.head.data;
+        }
+        throw new NullPointerException("Empty list");
+    }
+
+    public int search(int data){
+        int position = -1, count = 0;
+        if(this.head == null){
+            return position;
+        }
+        LLNode node = this.head;
+        while(node != null){
+            if(node.data == data){
+                position = count;
+                break;
+            }
+            count++;
+            node = node.next;
+        }
+        return position;
+    }
+
+}
